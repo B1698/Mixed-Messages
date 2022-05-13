6391918d42ee3be860c070b16f838455d01e3f9c
Bianca Anghelescu <103821609+B1698@users.noreply.github.com>
2022-05-13 17:05:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ function generateRandomNumber(num) {
   
   const generalBase = {
     mood: ['sad', 'stressed', 'happy', 'depressed'],
-    motivationalOutput: ['I am thankful for my struggle because without it I would not have come across my strength.', 
-    'You cannot start the next chapter of your life if you keep re-reading the last one.','There is only one happiness in this life, to love and be loved.',
-    'Even the darkest night will end and the sun will rise.'],
     advice: ['try to think of one or more things you are grateful for', 'Ground yourself, take a deep breath and find joy in the uniqueness of now', 'Be proud of yourself, to love and be loved is what makes this life magic.', 'Remember that this too shall pass, everything is temporary.']
   }
   
