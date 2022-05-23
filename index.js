9045449f086a7e2c2adcec61f53b01cc61e4bd28
Bianca Anghelescu <103821609+B1698@users.noreply.github.com>
2022-05-23 15:52:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 function generateRandomNumber(num) {
-    return Math.floor(Math.random() * num)
-  }
-  
-  const generalBase = {
-    mood: ['sad', 'stressed', 'happy', 'depressed'],
-    advice: ['try to think of one or more things you are grateful for', 'Ground yourself, take a deep breath and find joy in the uniqueness of now', 'Be proud of yourself, to love and be loved is what makes this life magic.', 'Remember that this too shall pass, everything is temporary.']
-  }
-  
-  let intimateBase = []
-  
-  
-  for(let prop in generalBase) {
-    let optionIdx = generateRandomNumber(generalBase[prop].length)
+return Math.floor(Math.random() * num)
+}
+const selectionGen = {
+  personality: ['friendly', 'sassy', 'evil', 'strong']
+}
+
+let personalityType = []
+for (let prop in selectionGen) {
+  let optionIdx = generateRandomNumber(selectionGen[prop].length)
+  switch(prop) {
+  case 'personality':
+  personalityType.push(`You are "${selectionGen[prop][optionIdx]}"`);
+  break;
+  default:
+  personalityType.push('Sorry, something went wrong!')
+};
 
 
-    switch(prop) {
-      case 'mood':
-        intimateBase.push(`If you are feeling "${generalBase[prop][optionIdx]}".`)
-        break
-      case 'advice':
-        intimateBase.push(`You should: "${generalBase[prop][optionIdx]}".`)
-        break
-      default:
-        intimateBase.push('Something went wrong.')
-    }
-  }
+function formatType(type) {
+  const formatted = personalityType.join('\n')
+  console.log(formatted)
+}
+formatType(personalityType);
+};
 
-  function formatBase(base) {
-
-    const formatted = intimateBase.join('\n')
-    console.log(formatted)
-  }
-  
-  formatBase(intimateBase);
